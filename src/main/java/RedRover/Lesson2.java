320ab0c5faa1ef4f02f24fc9cdbc326e79d19c2c
olgasuperstars <olgasuperstar@mail.ru>
2026-02-07 13:45:22 +0100

    Lesson 4 and edits of lesson 2

--- src/main/java/RedRover/Lesson2.java
+++ src/main/java/RedRover/Lesson2.java
@@ -22,16 +22,18 @@ public class Lesson2 {
         System.out.println("a = "+ a + "; b = " + b);
 
 
-        int team = 10;
-        double duoblon = 30;
-        double cash;
+        int team = 35;
+        int duoblon = 4000;
+        int cash;
         cash = duoblon / 2;
         System.out.println("Владелец корабля получит " + cash + " дублонов");
         cash = (duoblon - cash) / 2;
-        System.out.println("Капитан получит " + cash + " дублонов");
-        cash = ((duoblon - duoblon / 2) - cash) / team;
+        System.out.println("1 часть, которую получит Капитан " + cash + " дублонов");
+        cash = ((duoblon - duoblon / 2) - cash) / (team + 1);
         System.out.println("Члены команды получат по " + cash + " дублонов");
-        cash = (duoblon / 2) + ((duoblon - (duoblon / 2)) / 2);
+        cash = cash + ((duoblon - (duoblon / 2)) / 2);
+        System.out.println("Окончательный расчет Капитана после общего дележа " + cash + " дублонов");
+        cash = (duoblon / 2) + cash;
         System.out.println("Джек Воробей получит " + cash + " дублонов");
 
 
